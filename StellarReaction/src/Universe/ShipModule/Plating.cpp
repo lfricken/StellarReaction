a6b5e9a9425b95cd1f64727a31779283a342679d
Henry Lee <hlee145@illinois.edu>
2016-02-15 18:04:33 -0600

    Added in loop to check all graphics
    Testing to see the output for the vector holding all the graphics

--- StellarReaction/src/Universe/ShipModule/Plating.cpp
+++ StellarReaction/src/Universe/ShipModule/Plating.cpp
@@ -17,11 +17,17 @@ void Plating::directive(map<Directive, bool>& rIssues)
 	if (rIssues[Directive::Stealth])
 	{
 		//turn on stealth mode
-		stealthOn();
+		//stealthOn();
+    for(auto it = m_decors.begin(); it != m_decors.end(); ++it)
+    {
+      cout << it << endl;
+      sf::Color color = it->getColor();
+      it->setColor(sf::Color(color.r, color.g, color.b, 50));
+    }
 	}
 }
 
-void Plating::stealthOn() 
+void Plating::stealthOn()
 {
 	sf::Color color = m_parentChunk->getHull()->getColor();
 	//TODO: Make a constant for the alpha value of 50
