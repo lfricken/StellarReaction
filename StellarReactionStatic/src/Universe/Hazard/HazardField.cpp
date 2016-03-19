84be07a90503cb1e037cf7c2bb1d115efe9c8ba3
leon <fricken2@illinois.edu>
2016-03-18 20:38:59 -0500

    Removed hazard Field for testing

--- StellarReactionStatic/src/Universe/Hazard/HazardField.cpp
+++ StellarReactionStatic/src/Universe/Hazard/HazardField.cpp
@@ -17,28 +17,28 @@ HazardField::HazardField(Universe* universe, b2Vec2 origin)
 void HazardField::update()
 {
 
-	if(timer.isTimeUp())
-	{
-		b2Vec2 displacement = b2Vec2(static_cast<float>(rand() % 5), static_cast<float>(rand() % 5));
-		b2Vec2 velocity = b2Vec2(static_cast<float>(rand() % 10 - 5), static_cast<float>(rand() % 10 - 5));
-		//Get the asteroid's blueprint
-		ChunkData* p = universe->getBlueprints().getChunkSPtr("Asteroid")->clone();
-		//Move asteroid to a random location
+	//if(timer.isTimeUp())
+	//{
+	//	b2Vec2 displacement = b2Vec2(static_cast<float>(rand() % 5), static_cast<float>(rand() % 5));
+	//	b2Vec2 velocity = b2Vec2(static_cast<float>(rand() % 10 - 5), static_cast<float>(rand() % 10 - 5));
+	//	//Get the asteroid's blueprint
+	//	ChunkData* p = universe->getBlueprints().getChunkSPtr("Asteroid")->clone();
+	//	//Move asteroid to a random location
 
 
-		p->bodyComp.coords = origin + displacement;
-		Chunk* asteroid = p->generate(universe);
-		//To move :
-		asteroid->getBodyPtr()->SetLinearVelocity(velocity);
-		universe->add(asteroid);
-		timer.restartCountDown();
-		if (!spawned){
-			ChunkData* p = universe->getBlueprints().getChunkSPtr("DefaultBlackHole")->clone();
-			p->bodyComp.coords = b2Vec2(30, -30);
-			Chunk* hole = p->generate(universe);
-			universe->add(hole);
-			spawned = true;
-		}
-	}
+	//	p->bodyComp.coords = origin + displacement;
+	//	Chunk* asteroid = p->generate(universe);
+	//	//To move :
+	//	asteroid->getBodyPtr()->SetLinearVelocity(velocity);
+	//	universe->add(asteroid);
+	//	timer.restartCountDown();
+	//	if (!spawned){
+	//		ChunkData* p = universe->getBlueprints().getChunkSPtr("DefaultBlackHole")->clone();
+	//		p->bodyComp.coords = b2Vec2(30, -30);
+	//		Chunk* hole = p->generate(universe);
+	//		universe->add(hole);
+	//		spawned = true;
+	//	}
+	//}
 
 }
