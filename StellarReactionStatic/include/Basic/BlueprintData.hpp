2c15f480aaf50326f50bc770ce1091f1c5f9726a
Strikerklm96 <fricken2@illinois.edu>
2016-04-22 15:28:53 -0500

    Comments: Audio, Class Register, Blueprint Interface

--- StellarReactionStatic/include/Basic/BlueprintData.hpp
+++ StellarReactionStatic/include/Basic/BlueprintData.hpp
@@ -2,6 +2,7 @@
 
 #include "stdafx.hpp"
 
+//The interface for blueprint data.
 struct BlueprintData
 {
 	BlueprintData()
@@ -11,6 +12,7 @@ struct BlueprintData
 
 	std::string title;
 
+	//Fill this object with data from a json file.
 	virtual void loadJson(const Json::Value& root) = 0;
 };
 
--- StellarReactionStatic/include/Basic/ClassRegister.hpp
+++ StellarReactionStatic/include/Basic/ClassRegister.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
+//Create a register for this class with this name.
 #define MyType(BASE, TYPE) \
 	static Registerer<BASE, TYPE> reg
 
+//Register the class.
 #define RegisterClass(BASE, TYPE) \
 	Registerer<BASE, TYPE> TYPE::reg(#TYPE)
 
 template<class B>
+//Deduce which derived class a string refers to, given a base type.
 class Deduce
 {
 public:
+	//Deduce which derived class a string refers to, given a base type.
 	static B* from(const std::string& name, B* instance = NULL)
 	{
 		static std::map<std::string, B*> mapping;
@@ -23,10 +27,13 @@ public:
 	}
 };
 
+
 template<class B, class D>
+//Register a derived class.
 class Registerer
 {
 public:
+	//Register a class with this name.
 	Registerer(const std::string& name)
 	{
 		Deduce<B>::from(name, new D);//actually add
