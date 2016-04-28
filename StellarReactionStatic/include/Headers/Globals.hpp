71be290e1fac21975c1d9b789117c72032452ece
amcarth2 <amcarth2@illinois.edu>
2016-04-28 15:27:40 -0500

    Moved global definitions
    see isse #53

--- StellarReactionStatic/include/Headers/Globals.hpp
+++ StellarReactionStatic/include/Headers/Globals.hpp
@@ -8,11 +8,12 @@
 const std::string& contentDir();//directory of content
 const sf::Packet voidPacket;// a void packet when we don't want to send anything
 
-//TODO these should be defined here, but set somewhere else so changing them doesn't trigger a total recompile
-const int scale = 512;//1 box2d unit is scale pixels at 1 zoom
-const int standardModuleSize = 256;//size in pixels of module at 1 zoom
-const float sizeScalingFactor = static_cast<float>(standardModuleSize) / static_cast<float>(scale);// = 0.5 atm
-const float pi = 3.14159f;
+extern const int scale;//1 box2d unit is scale pixels at 1 zoom
+extern const int standardModuleSize;//size in pixels of module at 1 zoom
+extern const float sizeScalingFactor;// = 0.5 atm
+extern const float pi;
+
+
 #define FILELINE "file:{"<<__FILE__<<"}line:{"<<__LINE__<<"}"
 
 extern Game game;
--- StellarReactionStatic/src/Headers/Globals.cpp
+++ StellarReactionStatic/src/Headers/Globals.cpp
@@ -5,3 +5,8 @@ const std::string& contentDir()
 	static const std::string dir("../content/");
 	return dir;
 }
+
+const int scale = 512;//1 box2d unit is scale pixels at 1 zoom
+const int standardModuleSize = 256;//size in pixels of module at 1 zoom
+const float sizeScalingFactor = static_cast<float>(standardModuleSize) / static_cast<float>(scale);// = 0.5 atm
+const float pi = 3.14159f;
