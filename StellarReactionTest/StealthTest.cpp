64219ef24712dc8639f2d8da11f20bbeb1c072aa
Shaan <shaaniqbal75@gmail.com>
2016-04-28 18:56:32 -0500

    Added tests for Stealth
    Previous tests only checked if module was functioning. Added tests to
    check whether the ship is actually in stealth when it should be

--- StellarReactionTest/StealthTest.cpp
+++ StellarReactionTest/StealthTest.cpp
@@ -5,13 +5,34 @@
 
 extern Game game;
 
+TEST(StealthTest, StealthInactive)
+{
+	game.restartTest();
+
+	//Create Chunk
+	ChunkData testChunkData;
+
+	//Create Stealth
+	StealthData data;
+	data.energyConsumption = 0;
+	testChunkData.moduleData.push_back(sptr<ModuleData>(new StealthData(data)));
+
+	//Add Chunk
+	Chunk* testChunk = testChunkData.generate(&game.getUniverse());
+	game.getUniverse().add(testChunk);
+
+	game.runTicks(3);
+
+	//verify ship is not in stealth
+	EXPECT_FALSE(testChunk->isStealth());
+}
+
 TEST(StealthTest, StealthActive)
 {
 	game.restartTest();
 
 	//Create Chunk
 	ChunkData testChunkData;
-	testChunkData.ballisticData.Max = 22;
 
 	//Create Stealth
 	StealthData data;
@@ -27,5 +48,8 @@ TEST(StealthTest, StealthActive)
 	com.directives[Directive::Stealth] = true;
 	testChunk->getModuleList()[0]->directive(com);
 
-	EXPECT_TRUE(((ShipModule*)testChunk->getModuleList()[0].get())->isFunctioning());
+	game.runTicks(3);
+
+	//verify ship is in stealth mode
+	EXPECT_TRUE(testChunk->isStealth());
 }
