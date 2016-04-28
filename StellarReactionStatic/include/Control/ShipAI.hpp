7af86e660c02cc978a9810df11a98ca9b5debbae
dschlaht <dylan.schlaht@gmail.com>
2016-04-27 20:14:38 -0500

    More AI Changes
    -AI capture points until they are captured
    -AI won't capture a point if their team already owns it
    -AI shoot at nearby enemies while capturing and while in flight to the
    capture point

--- StellarReactionStatic/include/Control/ShipAI.hpp
+++ StellarReactionStatic/include/Control/ShipAI.hpp
@@ -14,22 +14,25 @@ public:
 	~ShipAI();
 
 	void updateDecision();
-	
 
 protected:
 private:
-	bool isStuck(b2Vec2 curPos);
-	Timer m_stuckTimer;
-	Timer m_unstuckTimer;
+	bool isStuck(b2Vec2 curPos);//check if we are stuck
+	Timer m_stuckTimer; //how often we check our position to see if we are stuck
+	Timer m_unstuckTimer; //how long we go in reverse to get unstuck
 	b2Vec2 oldPos;
 
-	Timer m_targetTimer;
-	void flyTowardsTarget();
-	void fireAtTarget();
+	Timer m_huntingTimer; //timer for how long we should hunt enemies for
+	Timer m_targetTimer; //timer for switching to a closer enemy
+	void flyTowardsChunk(Chunk* target);//fly ship towards chunk
+	void fireAtTarget();//fire at m_pCurrentTarget
 
 
-	int m_currentBehavior;
-	Chunk* m_pCurrentTarget;
+	int m_currentBehavior;//our current behavior from 0 to m_numBehaviors
+	int m_numBehaviors;//number of behaviors, includes stuck behavior which should always be last
+	Chunk* m_pCurrentTarget;//our current enemy target nearest to us
+	Chunk* m_pCurrentDest;//our current destination to fly to
+	
 	Map<Directive, bool> m_directives;
 	Map<int, bool> m_weaponGroups;
 };
--- StellarReactionStatic/src/Control/ShipAI.cpp
+++ StellarReactionStatic/src/Control/ShipAI.cpp
@@ -5,13 +5,17 @@
 #include "Chunk.hpp"
 #include "Convert.hpp"
 #include "CommandInfo.hpp"
+#include "CaptureArea.hpp"
 
 using namespace std;
 
 ShipAI::ShipAI(int team, int controller_index) : BasePlayerTraits("ai")
 {
 	m_currentBehavior = 2;
+	m_numBehaviors = 2;
 	m_pCurrentTarget = NULL;
+	m_pCurrentDest = NULL;
+	m_huntingTimer.setCountDown((rand() % 15) + 15);
 	m_targetTimer.setCountDown(5.f);
 	m_stuckTimer.setCountDown(2.f);
 	m_unstuckTimer.setCountDown(1.f);
@@ -39,67 +43,110 @@ void ShipAI::updateDecision()
 
 	//check if stuck
 	if ( isStuck(pBody->GetPosition()) ){
-		m_currentBehavior = 1;
+		m_currentBehavior = 2;
 		m_unstuckTimer.restartCountDown();
 	}
 	//update directives based on current behavior
 	if (m_currentBehavior == 0) // we should attack enemy players
 	{
-		if (m_targetTimer.isTimeUp() || m_pCurrentTarget == NULL) {
+		//check for a new target
+		if (m_targetTimer.isTimeUp()) {
 			m_targetTimer.restartCountDown();
 			int teams[] = {1,2,3,4};
 			std::list<int> teamList(teams, teams + 4);
 			teamList.remove(getTeam());
 			m_pCurrentTarget = game.getUniverse().getNearestChunkOnTeam(pBody->GetPosition(), rController.getSlave(), teamList);
 		}
-
-
+		//fire at and fly toward target if we have one
 		if (m_pCurrentTarget != NULL)
 		{
 			b2Vec2 targetPos = m_pCurrentTarget->getBodyPtr()->GetPosition();
 			rController.setAim(targetPos);
 
-			flyTowardsTarget();
+			flyTowardsChunk(m_pCurrentTarget);
 			fireAtTarget();
 			m_directives[Directive::Up] = true;
 		}
+		//check if we have been hunting other players for long enough
+		if (m_huntingTimer.isTimeUp())
+		{
+			int new_behavior = rand() % m_numBehaviors;
+			cout << new_behavior;
+			if (new_behavior != m_currentBehavior)
+			{
+				m_currentBehavior = new_behavior;
+				m_pCurrentTarget = NULL;
+			}
+			m_huntingTimer.restartCountDown();
+		}
+
 
 	}
-	else if (m_currentBehavior == 1) //we are stuck
+	else if (m_currentBehavior == 1) //find station to capture
 	{
+		if (m_pCurrentDest == NULL) {
+			m_pCurrentDest = game.getUniverse().getNearestStation(pBody->GetPosition(), getTeam());
+		}
+
+		if (m_pCurrentDest != NULL)
+		{
+			flyTowardsChunk(m_pCurrentDest);
+			m_directives[Directive::Up] = true;
+
+			//check if capture area has been captured by this AI or our team
+			CaptureArea* m_currentCapArea = dynamic_cast<CaptureArea*>((&*m_pCurrentDest->getModuleList()[0]));
+			if (m_currentCapArea!= NULL && m_currentCapArea->getProgress() >= 1.0f)
+			{
+				m_pCurrentTarget = NULL;
+				m_pCurrentDest = NULL;
+				m_currentBehavior = rand() % m_numBehaviors;
+			}
+
+			//check for enemies along the way
+			if (m_targetTimer.isTimeUp()) {
+				m_targetTimer.restartCountDown();
+				int teams[] = { 1, 2, 3, 4 };
+				std::list<int> teamList(teams, teams + 4);
+				teamList.remove(getTeam());
+				m_pCurrentTarget = game.getUniverse().getNearestChunkOnTeam(pBody->GetPosition(), rController.getSlave(), teamList);
+			}
+
+			//fire at enemies along the way, but don't fly toward them
+			if (m_pCurrentTarget != NULL)
+			{
+				b2Vec2 targetPos = m_pCurrentTarget->getBodyPtr()->GetPosition();
+				rController.setAim(targetPos);
+
+				fireAtTarget();
+			}
+		}
+
+	}
+	else if (m_currentBehavior == 2) //we are stuck
+	{
+		//go in reverse for the duration of m_unstuckTimer
 		m_directives[Directive::Down] = true;
 		if (m_unstuckTimer.isTimeUp()){
-			m_currentBehavior = 0;
+			m_pCurrentTarget = NULL;
+			m_pCurrentDest = NULL;
+			m_currentBehavior = rand() % m_numBehaviors;
 		}
 	}
-	else if (m_currentBehavior == 2) //find station to capture
-	{
-		if (m_pCurrentTarget == NULL) {
-			m_pCurrentTarget = game.getUniverse().getNearestStation(pBody->GetPosition(), getTeam());
-		}
-
-		if (m_pCurrentTarget != NULL)
-		{
-			flyTowardsTarget();
-			m_directives[Directive::Up] = true;
-		}
-	}
-
+	//send commands to controller to move the ship
 	CommandInfo commands;
 	commands.isLocal = true;
 	commands.directives = m_directives;
 	commands.weaponGroups = m_weaponGroups;
-
 	rController.locallyUpdate(commands);
 }
-void ShipAI::flyTowardsTarget()
+void ShipAI::flyTowardsChunk(Chunk* target)
 {
 	Controller& rController = game.getUniverse().getControllerFactory().getController(m_controller);
 	b2Body* pBody = rController.getBodyPtr();
 
 	float ourAngle = leon::normRad(pBody->GetAngle() + pi/2);
 	b2Vec2 ourPos = pBody->GetPosition();
-	b2Vec2 targetPos = m_pCurrentTarget->getBodyComponent().getPosition();
+	b2Vec2 targetPos = target->getBodyComponent().getPosition();
 
 	b2Vec2 diff = targetPos - ourPos;
 
@@ -109,7 +156,7 @@ void ShipAI::flyTowardsTarget()
 
 	float diffAngle = leon::normRad(targetAngle - ourAngle);
 
-	if (dist > 15 || m_currentBehavior == 2)
+	if (dist > 15 || m_currentBehavior == 1)
 	{
 		//angle toward target
 		if (diffAngle < pi)
@@ -128,15 +175,20 @@ void ShipAI::flyTowardsTarget()
 			perpVec = b2Vec2(diff.y, -diff.x);
 		}
 		else
+		{
 			perpVec = b2Vec2(-diff.y, diff.x);
+		}
+
 		targetAngle = leon::normRad(atan2(perpVec.y, perpVec.x));
 		diffAngle = leon::normRad(targetAngle - ourAngle);
 		if (diffAngle < pi)
 		{
 			m_directives[Directive::RollCCW] = true;
-		}
+		} 
 		else
+		{
 			m_directives[Directive::RollCW] = true;
+		}
 	}
 
 }
