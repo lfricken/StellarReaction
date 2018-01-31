f0175705a80e734b5bc42ec6618c0fd6f87c329d
Leon <Strikerklm96@gmail.com>
2018-01-30 22:21:41 -0600

    wtf why git

--- StellarReactionStatic/include/Universe/BuildingModules/Extractor.hpp
+++ StellarReactionStatic/include/Universe/BuildingModules/Extractor.hpp
@@ -5,7 +5,7 @@
 struct ExtractorData;
 
 /// <summary>
-/// Module that represents the extractor.
+/// Module that changes income.
 /// </summary>
 class Extractor : public ShipModule
 {
@@ -15,20 +15,23 @@ public:
 	virtual ~Extractor();
 
 protected:
+
+	bool m_opposingTeams; // true if this change in resources is applied to the opposing teams
+	Resources m_incomeChange; // how much income does this object add or subtract from its team?
 private:
 
 };
 
-/// <summary>
-/// 
-/// </summary>
 struct ExtractorData : ShipModuleData
 {
 	ExtractorData() :
-		ShipModuleData()
-
+		ShipModuleData(),
+		opposingTeams(false)
 	{
 	}
+
+	bool opposingTeams; // true if this change in resources is applied to the opposing teams
+	Resources incomeChange; // how much income does this object add or subtract from its team just by existing?
 
 	///Create Capacitor object from this data object.
 	virtual Module* generate(GenerateParams params) const
